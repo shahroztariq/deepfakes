8139a13d2bd4bd1a9278612ff0f2a6179e2bef1c
shahroztariq <shahroztariq@gmail.com>
2026-05-27 22:41:03 +1000

    chore: migrate routing to HashRouter and configure build settings for GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Publications from './pages/Publications';
 import Explorer from './pages/Explorer';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navigation />
       <Routes>
         <Route path="/" element={<Publications />} />
         <Route path="/explorer" element={<Explorer />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
